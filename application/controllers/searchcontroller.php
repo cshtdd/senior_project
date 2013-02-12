8f36d1f5ccdfd91381c1ba02007894d0231241c4
Camilo Sanchez <camilin87@gmail.com>
2013-02-11 21:42:55 -0500

    test data for search_controller search method

--- application/controllers/searchcontroller.php
+++ application/controllers/searchcontroller.php
@@ -2,31 +2,82 @@
 
 class SearchController extends CI_Controller 
 {
-	/*
+	private $is_test = true;
+
 	public function __construct()
 	{
-        parent::__construct();
-		$this->output->cache(60);
+		parent::__construct();
+
+		$this->load->model('Project_Summary_Model');
+
+		//$this->output->cache(60);
 	}
-	*/
+	
 	
 	public function search($search_param='')
 	{
 		if (isset($search_param) && strlen($search_param) > 0)
 		{
 			$search_query = urldecode($search_param);
-		
-			//$this->output->set_output("new search query: '$search_query'");
-			$data['no_results'] = false;
+
+			$lProjects = $this->getProjectsWithSearchParam($search_query);
+
+			if (isset($lProjects) && count($lProjects) > 0)
+			{
+				$data['lProjects'] = $lProjects;
+				$data['no_results'] = false;
+			}
+			else
+			{
+				$data['no_results'] = true;
+			}
 		}
 		else
 		{
-			//$this->output->set_output('new no search query');
 			$data['no_results'] = true;
 		}
 
 		$this->load->view('search_controller_search', $data);
 	}
-	
-	
+
+	private function getProjectsWithSearchParam($search_query)
+	{
+		if ($this->is_test)
+		{
+			return $this->getProjectsWithSearchParamTest($search_query);
+		}
+		else
+		{
+			throw new Exception('not implemented'); 
+		}
+	}
+	private function getProjectsWithSearchParamTest($search_query)
+	{
+		$project1 = new Project_Summary_Model();
+		$project1->project_id = 1;
+		$project1->project_title = 'Free Music Sharing Platform';
+		$project1->project_short_description = 'Poor students need an easy way to access all the music in the world for free.';
+		$project1->project_close_date = '02-28-2013';
+
+		$project2 = new Project_Summary_Model();
+		$project2->project_id = 2;
+		$project2->project_title = 'Moodle on Facebook';
+		$project2->project_short_description = 'Poor students need an easy way to access all the music in the world for free. This Project will make every student really happy.';
+		$project2->project_close_date = '02-28-2013';
+
+		$lProjects = array(
+			$project1, 
+			$project2,
+			$project1, 
+			$project2,
+			$project1, 
+			$project2,
+			$project1, 
+			$project2,
+			$project1, 
+			$project2
+		);
+
+		return $lProjects;
+	}
 }
