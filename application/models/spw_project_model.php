2042bfbb86320919a855cf342ec4e8ee6e261807
Camilo Sanchez <camilin87@gmail.com>
2013-02-17 10:59:26 -0500

    project model fields documented

--- application/models/spw_project_model.php
+++ application/models/spw_project_model.php
@@ -6,8 +6,12 @@ class SPW_Project_Model extends CI_Model
 	public $title;
 	public $description;
 	public $max_students;
+
+	//the id of the SPW_User
 	public $proposed_by;
+	//the id of the SPW_Term
 	public $delivery_term;
+	//The id of the SPW_Project_Status
 	public $status;
 
 	//TODO: remove this later
