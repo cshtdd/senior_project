22d1e6d5db1fb4cafbc7d8a8f065a6b51a3b2b0a
Camilo Sanchez <camilin87@gmail.com>
2013-02-17 16:31:15 -0500

    getFullName added to user summary view model

--- application/models/spw_user_summary_view_model.php
+++ application/models/spw_user_summary_view_model.php
@@ -5,11 +5,14 @@ class SPW_User_Summary_View_Model extends CI_Model
 	//a SPW_User_Model object
 	public $user;
 
-	public $full_name;
-
 	public function __construct()
 	{
 		parent::__construct();
+	}
+
+	public function getFullName()
+	{
+		return ucwords($this->user->first_name.' '.$this->user->last_name);
 	}
 }
 	
