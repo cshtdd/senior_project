5ddc535b20493b83bce7d759e261fc6b2fca1fe8
Camilo Sanchez <camilin87@gmail.com>
2013-02-17 11:08:29 -0500

    spw_skill class

---
+++ application/models/spw_skill_model.php
@@ -0,0 +1,16 @@
+<?php
+
+class SPW_Skill_Model extends CI_Model
+{
+	public $id;
+	public $name;
+	//IsEnabled?
+	public $website_active;
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+}
+	
+?>
