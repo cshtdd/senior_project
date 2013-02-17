aed5be132d410ea69b932e8f6a46749f248f6997
Camilo Sanchez <camilin87@gmail.com>
2013-02-17 10:43:14 -0500

    all db fields added to class

--- application/models/spw_project_model.php
+++ application/models/spw_project_model.php
@@ -5,6 +5,12 @@ class SPW_Project_Model extends CI_Model
 	public $id;
 	public $title;
 	public $description;
+	public $max_students;
+	public $proposed_by;
+	public $delivery_term;
+	public $status;
+
+	//remove this later
 	public $project_close_date;
 	
 	public function __construct()
@@ -13,4 +19,4 @@ class SPW_Project_Model extends CI_Model
 	}
 }
 	
-?> 
+?>
