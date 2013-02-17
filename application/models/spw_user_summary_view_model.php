401d1e97f52f013cc2b0d831056ebf908d0c91e3
Camilo Sanchez <camilin87@gmail.com>
2013-02-17 11:31:47 -0500

    SPW_User_Summary_View_Model class created

---
+++ application/models/spw_user_summary_view_model.php
@@ -0,0 +1,16 @@
+<?php
+
+class SPW_User_Summary_View_Model extends CI_Model
+{
+	//a SPW_User_Model object
+	public $user;
+
+	public $full_name;
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+}
+	
+?>
