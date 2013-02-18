2ca5e156c685f5b2dd221cb7f22e74277d79cefe
Camilo Sanchez <camilin87@gmail.com>
2013-02-18 17:33:22 -0500

    no anchor url helper

--- application/views/user_summary_full_name.php
+++ application/views/user_summary_full_name.php
@@ -1,2 +1,8 @@
 <?php $this->load->helper('url'); ?>
-<b><?php echo anchor('user/'.$user_summary->user->id, $user_summary->getFullName()) ?></b>
+
+<!-- this didn't work, anchor keeps adding the stupid index.php -->
+<!--<b><?php echo anchor('user/'.$user_summary->user->id, $user_summary->getFullName()) ?></b> -->
+
+<a href="<?php echo base_url().'user/'.$user_summary->user->id ?>">
+	<?php echo $user_summary->getFullName() ?>
+</a>
