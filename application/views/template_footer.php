8c4a482f8b8aaa4f6ce78f02d75d0329d12f0f95
Camilo Sanchez <camilin87@gmail.com>
2013-02-18 18:34:31 -0500

    bootstrap added

--- application/views/template_footer.php
+++ application/views/template_footer.php
@@ -1,4 +1,7 @@
 </div>
 <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+
+	<script src="http://code.jquery.com/jquery.js"></script>
+	<script src="<?php echo base_url() ?>js/bootstrap.min.js"></script>
 </body>
 </html>
--- application/views/template_header.php
+++ application/views/template_header.php
@@ -3,6 +3,13 @@
 <head>
 	<meta charset="utf-8">
 	<title>CASH Productions</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+
+    <link href="<?php echo base_url() ?>css/bootstrap.min.css" rel="stylesheet" media="screen">
+
+    <?php  ?>
 </head>	
 <body>
 	<div id="mainBody">
