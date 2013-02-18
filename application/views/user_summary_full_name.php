fc65441a25fd556d1c80d62b7450b763495f396b
Camilo Sanchez <camilin87@gmail.com>
2013-02-18 17:33:52 -0500

    unnecessary comment trimmed

--- application/views/user_summary_full_name.php
+++ application/views/user_summary_full_name.php
@@ -1,8 +1,5 @@
 <?php $this->load->helper('url'); ?>
 
-<!-- this didn't work, anchor keeps adding the stupid index.php -->
-<!--<b><?php echo anchor('user/'.$user_summary->user->id, $user_summary->getFullName()) ?></b> -->
-
 <a href="<?php echo base_url().'user/'.$user_summary->user->id ?>">
 	<?php echo $user_summary->getFullName() ?>
 </a>
