2215c7b07aec560c42ecb17116759450a3bb811e
Camilo Sanchez <camilin87@gmail.com>
2013-02-18 20:03:28 -0500

    search layout cleared up a lil bit

--- application/views/search_controller_search.php
+++ application/views/search_controller_search.php
@@ -11,60 +11,62 @@
 			<lh><h2>Project Results</h2></lh>
 			<?php foreach ($lProjects as $iProject) { ?>
 				<li>
-					<div class="right"> <?php echo $iProject->getlSkillNames() ?> </div>
-					<h4><?php echo $iProject->project->title ?></h4>
+					<div class="project_summary">
+						<div class="right"> <?php echo $iProject->getlSkillNames() ?> </div>
+						<h4><?php echo $iProject->project->title ?></h4>
 
-					<p>
-						<?php echo $iProject->getShortDescription() ?>
-						<a href="<?php echo base_url().'project/'.$iProject->project->id ?>">More Info...</a>
-					</p>
+						<p>
+							<?php echo $iProject->getShortDescription() ?>
+							<a href="<?php echo base_url().'project/'.$iProject->project->id ?>">More Info...</a>
+						</p>
 
-					<ul class="unstyled inline">
-						<lh>Team Leader:</lh>
-						<li>
-							<?php $this->load->view('user_summary_full_name', array('user_summary' => $iProject->teamLeaderSummary) )?>
-						</li>
-					</ul>
+						<ul class="unstyled inline">
+							<lh>Team Leader:</lh>
+							<li>
+								<?php $this->load->view('user_summary_full_name', array('user_summary' => $iProject->teamLeaderSummary) )?>
+							</li>
+						</ul>
 
-					<ul class="unstyled inline">
-						<lh>Team Mentors:</lh>
+						<ul class="unstyled inline">
+							<lh>Team Mentors:</lh>
 
-						<?php if (isset($iProject->lMentorSummaries) && count($iProject->lMentorSummaries) > 0) { ?>
-							
-							<?php foreach ($iProject->lMentorSummaries as $iMentorSumm) { ?>
-								<li>
-									<?php $this->load->view('user_summary_full_name', array('user_summary' => $iMentorSumm) )?>
-								</li>
-							<?php } ?>
+							<?php if (isset($iProject->lMentorSummaries) && count($iProject->lMentorSummaries) > 0) { ?>
+								
+								<?php foreach ($iProject->lMentorSummaries as $iMentorSumm) { ?>
+									<li>
+										<?php $this->load->view('user_summary_full_name', array('user_summary' => $iMentorSumm) )?>
+									</li>
+								<?php } ?>
 
-						<?php } else { ?>
+							<?php } else { ?>
 
-							<li>This team needs a mentor...</li>
+								<li>This team needs a mentor...</li>
 
-						<?php }?>
-					</ul>
+							<?php }?>
+						</ul>
 
 
-					<ul class="unstyled inline">
-						<lh>Team Members:</lh>
+						<ul class="unstyled inline">
+							<lh>Team Members:</lh>
 
-						<?php if (isset($iProject->lTeamMemberSummaries) && count($iProject->lTeamMemberSummaries) > 0) { ?>
+							<?php if (isset($iProject->lTeamMemberSummaries) && count($iProject->lTeamMemberSummaries) > 0) { ?>
 
-							<?php foreach ($iProject->lTeamMemberSummaries as $iMemberSumm) { ?>
-								<li>
-									<?php $this->load->view('user_summary_full_name', array('user_summary' => $iMemberSumm) )?>
-								</li>
-							<?php } ?>
+								<?php foreach ($iProject->lTeamMemberSummaries as $iMemberSumm) { ?>
+									<li>
+										<?php $this->load->view('user_summary_full_name', array('user_summary' => $iMemberSumm) )?>
+									</li>
+								<?php } ?>
 
-						<?php } else { ?>
+							<?php } else { ?>
 
-							<li>Join this team for a bree beer! Not really...</li>
+								<li>Join this team for a free beer! Not really...</li>
 
-						<?php }?>
-					</ul>
+							<?php }?>
+						</ul>
 
-					Close Date: <?php echo $iProject->term->end_date ?>
-					Project Status: <?php echo $iProject->project->status->name ?>
+						Close Date: <?php echo $iProject->term->end_date ?>
+						Project Status: <?php echo $iProject->project->status->name ?>
+					</div>
 				</li>
 			<?php } ?>
 		</ul>
