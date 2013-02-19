6634ac0d6b59aeea0851ef693073b117bc760ac3
Camilo Sanchez <camilin87@gmail.com>
2013-02-18 23:49:13 -0500

    well for project summaries

--- application/views/search_controller_search.php
+++ application/views/search_controller_search.php
@@ -10,67 +10,65 @@
 		<ul class="project_list">
 			<lh><h2>Project Results</h2></lh>
 			<?php foreach ($lProjects as $iProject) { ?>
-				<li>
-					<div class="project_summary well">
-						<div class="pull-right"> <?php echo $iProject->getlSkillNames() ?> </div>
-						<h4>
-							<a href="<?php echo base_url().'project/'.$iProject->project->id ?>">
-								<?php echo $iProject->project->title ?>
-							</a>
-						</h4>
+				<li class="well">
+					<div class="pull-right"> <?php echo $iProject->getlSkillNames() ?> </div>
+					<h4>
+						<a href="<?php echo base_url().'project/'.$iProject->project->id ?>">
+							<?php echo $iProject->project->title ?>
+						</a>
+					</h4>
 
-						<p>
-							<?php echo $iProject->getShortDescription() ?>
-							<a href="<?php echo base_url().'project/'.$iProject->project->id ?>">More Info...</a>
-						</p>
+					<p>
+						<?php echo $iProject->getShortDescription() ?>
+						<a href="<?php echo base_url().'project/'.$iProject->project->id ?>">More Info...</a>
+					</p>
 
-						<ul class="unstyled inline">
-							<lh>Team Leader:</lh>
-							<li>
-								<?php $this->load->view('user_summary_full_name', array('user_summary' => $iProject->teamLeaderSummary) )?>
-							</li>
-						</ul>
+					<ul class="unstyled inline">
+						<lh>Team Leader:</lh>
+						<li>
+							<?php $this->load->view('user_summary_full_name', array('user_summary' => $iProject->teamLeaderSummary) )?>
+						</li>
+					</ul>
 
-						<ul class="unstyled inline">
-							<lh>Team Mentors:</lh>
+					<ul class="unstyled inline">
+						<lh>Team Mentors:</lh>
 
-							<?php if (isset($iProject->lMentorSummaries) && count($iProject->lMentorSummaries) > 0) { ?>
-								
-								<?php foreach ($iProject->lMentorSummaries as $iMentorSumm) { ?>
-									<li>
-										<?php $this->load->view('user_summary_full_name', array('user_summary' => $iMentorSumm) )?>
-									</li>
-								<?php } ?>
+						<?php if (isset($iProject->lMentorSummaries) && count($iProject->lMentorSummaries) > 0) { ?>
+							
+							<?php foreach ($iProject->lMentorSummaries as $iMentorSumm) { ?>
+								<li>
+									<?php $this->load->view('user_summary_full_name', array('user_summary' => $iMentorSumm) )?>
+								</li>
+							<?php } ?>
 
-							<?php } else { ?>
+						<?php } else { ?>
 
-								<li>This team needs a mentor...</li>
+							<li>This team needs a mentor...</li>
 
-							<?php }?>
-						</ul>
+						<?php }?>
+					</ul>
 
 
-						<ul class="unstyled inline">
-							<lh>Team Members:</lh>
+					<ul class="unstyled inline">
+						<lh>Team Members:</lh>
 
-							<?php if (isset($iProject->lTeamMemberSummaries) && count($iProject->lTeamMemberSummaries) > 0) { ?>
+						<?php if (isset($iProject->lTeamMemberSummaries) && count($iProject->lTeamMemberSummaries) > 0) { ?>
 
-								<?php foreach ($iProject->lTeamMemberSummaries as $iMemberSumm) { ?>
-									<li>
-										<?php $this->load->view('user_summary_full_name', array('user_summary' => $iMemberSumm) )?>
-									</li>
-								<?php } ?>
+							<?php foreach ($iProject->lTeamMemberSummaries as $iMemberSumm) { ?>
+								<li>
+									<?php $this->load->view('user_summary_full_name', array('user_summary' => $iMemberSumm) )?>
+								</li>
+							<?php } ?>
 
-							<?php } else { ?>
+						<?php } else { ?>
 
-								<li>Join this team for a free beer! Not really...</li>
+							<li>Join this team for a free beer! Not really...</li>
 
-							<?php }?>
-						</ul>
+						<?php }?>
+					</ul>
 
-						Close Date: <?php echo $iProject->term->end_date ?>
-						Project Status: <?php echo $iProject->project->status->name ?>
-					</div>
+					Close Date: <?php echo $iProject->term->end_date ?>
+					Project Status: <?php echo $iProject->project->status->name ?>
 				</li>
 			<?php } ?>
 		</ul>
