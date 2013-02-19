7b019a8f0d554be33845f146d4400d1145803fad
Camilo Sanchez <camilin87@gmail.com>
2013-02-18 23:12:07 -0500

    latest query referenced

--- application/views/template_footer.php
+++ application/views/template_footer.php
@@ -8,7 +8,7 @@
 	</div>
 </div>
 
-	<script src="http://code.jquery.com/jquery.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 	<script src="<?php echo base_url() ?>js/bootstrap.min.js"></script>
 </body>
 </html>
