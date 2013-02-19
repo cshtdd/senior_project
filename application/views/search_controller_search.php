65fdb283685858c57bb228e3b067e2eb6ec557e7
Camilo Sanchez <camilin87@gmail.com>
2013-02-18 19:31:13 -0500

    hor_list class

--- application/views/search_controller_search.php
+++ application/views/search_controller_search.php
@@ -11,52 +11,51 @@
 			<lh><h2>Project Results</h2></lh>
 			<?php foreach ($lProjects as $iProject) { ?>
 				<li>
-						<h4><?php echo $iProject->project->title ?></h4>
-						<br/><?php echo $iProject->getShortDescription() ?>
-						<a href="<?php echo base_url().'project/'.$iProject->project->id ?>">More Info...</a>
+					<h4><?php echo $iProject->project->title ?></h4>
+					<br/><?php echo $iProject->getShortDescription() ?>
+					<a href="<?php echo base_url().'project/'.$iProject->project->id ?>">More Info...</a>
 
-						<br/><?php echo $iProject->getlSkillNames() ?>
-						<br/>Close Date: <?php echo $iProject->term->end_date ?>
-						<br/>Project Status: <?php echo $iProject->project->status->name ?>
-						<br/>Team Leader: <?php $this->load->view('user_summary_full_name', array('user_summary' => $iProject->teamLeaderSummary) )?>
+					<br/><?php echo $iProject->getlSkillNames() ?>
+					<br/>Close Date: <?php echo $iProject->term->end_date ?>
+					<br/>Project Status: <?php echo $iProject->project->status->name ?>
+					<br/>Team Leader: <?php $this->load->view('user_summary_full_name', array('user_summary' => $iProject->teamLeaderSummary) )?>
 
-						<ul>
-							<lh><h5>Team Mentors:</h5></lh>
+					<ul class="hor_list">
+						<lh>Team Mentors:</lh>
 
-							<?php if (isset($iProject->lMentorSummaries) && count($iProject->lMentorSummaries) > 0) { ?>
-								
-								<?php foreach ($iProject->lMentorSummaries as $iMentorSumm) { ?>
-									<li>
-										<?php $this->load->view('user_summary_full_name', array('user_summary' => $iMentorSumm) )?>
-									</li>
-								<?php } ?>
+						<?php if (isset($iProject->lMentorSummaries) && count($iProject->lMentorSummaries) > 0) { ?>
+							
+							<?php foreach ($iProject->lMentorSummaries as $iMentorSumm) { ?>
+								<li>
+									<?php $this->load->view('user_summary_full_name', array('user_summary' => $iMentorSumm) )?>
+								</li>
+							<?php } ?>
 
-							<?php } else { ?>
+						<?php } else { ?>
 
-								<li>This team needs a mentor...</li>
+							<li>This team needs a mentor...</li>
 
-							<?php }?>
-						</ul>
+						<?php }?>
+					</ul>
 
 
+					<ul class="hor_list">
+						<lh>Team Members:</lh>
 
-						<ul>
-							<lh><h5>Team Members:</h5></lh>
+						<?php if (isset($iProject->lTeamMemberSummaries) && count($iProject->lTeamMemberSummaries) > 0) { ?>
 
-							<?php if (isset($iProject->lTeamMemberSummaries) && count($iProject->lTeamMemberSummaries) > 0) { ?>
+							<?php foreach ($iProject->lTeamMemberSummaries as $iMemberSumm) { ?>
+								<li>
+									<?php $this->load->view('user_summary_full_name', array('user_summary' => $iMemberSumm) )?>
+								</li>
+							<?php } ?>
 
-								<?php foreach ($iProject->lTeamMemberSummaries as $iMemberSumm) { ?>
-									<li>
-										<?php $this->load->view('user_summary_full_name', array('user_summary' => $iMemberSumm) )?>
-									</li>
-								<?php } ?>
+						<?php } else { ?>
 
-							<?php } else { ?>
+							<li>Join this team for a bree beer! Not really...</li>
 
-								<li>Join this team for a bree beer! Not really...</li>
-
-							<?php }?>
-						</ul>
+						<?php }?>
+					</ul>
 				</li>
 			<?php } ?>
 		</ul>
