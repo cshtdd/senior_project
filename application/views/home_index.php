370e3f4454d54cd0b5333897389e655720f69fcf
Camilo Sanchez <camilin87@gmail.com>
2013-02-19 21:18:07 -0500

    home controller displays the list of projects

--- application/views/home_index.php
+++ application/views/home_index.php
@@ -1,7 +1,21 @@
 <?php $this->load->view("template_header"); ?>
 
-<p>
-	woo hoo it works this is the new home
-</p>
+<?php if ($no_results) { ?>
+
+	<p>We could not match you with anything</p>
+
+<?php } else { ?>
+
+
+	<?php if (isset($lSuggestedProjects) && count($lSuggestedProjects) > 0) { ?>
+		<?php $this->load->view('project_summary_list', array('lProjects' => $lSuggestedProjects, 'list_title' => 'Suggested Projects') ) ?>
+		<hr>
+	<?php } ?>
+
+	<?php if (isset($lRegularProjects) && count($lRegularProjects) > 0) { ?>
+		<?php $this->load->view('project_summary_list', array('lProjects' => $lRegularProjects, 'list_title' => '') )?>
+	<?php } ?>
+
+<?php }?>
 	
 <?php $this->load->view("template_footer"); ?>
