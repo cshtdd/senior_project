db89aa6a336d869f7abae4cd66cf56f458a56d75
Camilo Sanchez <camilin87@gmail.com>
2013-02-19 22:53:00 -0500

    past_projects view implemented

---
+++ application/views/past_projects.php
@@ -0,0 +1,15 @@
+<?php $this->load->view("template_header"); ?>
+
+<?php if ($no_results) { ?>
+
+	<p>There are no past projects</p>
+
+<?php } else { ?>
+
+	<?php if (isset($lProjects) && count($lProjects) > 0) { ?>
+		<?php $this->load->view('project_summary_list', array('lProjects' => $lProjects, 'list_title' => 'Past Projects') )?>
+	<?php } ?>
+
+<?php }?>
+	
+<?php $this->load->view("template_footer"); ?>
