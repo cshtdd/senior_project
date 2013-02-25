8e6899ab2bcce70305ec600edc55ea3be92ab832
Keiser Moya <kmoya002@fiu.edu>
2013-02-25 17:07:51 -0500

    project model
    started working on

--- application/models/spw_project_model.php
+++ application/models/spw_project_model.php
@@ -8,9 +8,11 @@ class SPW_Project_Model extends CI_Model
 	public $max_students;
 
 	//the id of the SPW_User
-	public $proposed_by;
+	public $team_leader;
+
 	//the id of the SPW_Term
 	public $delivery_term;
+
 	//The id of the SPW_Project_Status
 	public $status;
 
@@ -18,6 +20,46 @@ class SPW_Project_Model extends CI_Model
 	{
 		parent::__construct();
 	}
+
+	public function getRegularProjectIds($lSuggestedProjectIds)
+	{
+		$lenght = count($lSuggestedProjectIds);
+
+		$count = 0;
+
+		$sql = 'select spw_project.id
+	         	from spw_project, spw_term
+	         	where (spw_project.status = 3) and (spw_term.id = spw_project.delivery_term) 
+	                   and (spw_term.closed_requests > NOW())
+             	order by id ASC';
+
+        $query = $this->db->query($sql);
+
+        sort($lSuggestedProjectIds);
+
+        $res = array();
+
+        foreach ($query->result() as $row)
+		{
+			while ($lSuggestedProjectIds[$count] < $row->id)
+			{
+				count++;
+			}
+
+			if ()
+			{
+
+			}
+			else
+			{
+				
+			}
+
+		}
+
+		return $res;
+	}
+
 }
 	
 ?>
