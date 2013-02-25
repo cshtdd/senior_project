b7fe978477fe8d9802c0356a5c3db12e6be3fd38
Keiser Moya <kmoya002@fiu.edu>
2013-02-25 17:04:41 -0500

    Matching projects given user
    functions to check if a user have selected a project already, determine
    which projects are suitable to be suggested and the matching skill
    projects given a user

--- application/models/spw_user_model.php
+++ application/models/spw_user_model.php
@@ -41,6 +41,73 @@ class SPW_User_Model extends CI_Model
 		}
 	}
 	
+
+	/* return the list of suggested projects IDs with the highest matches having in
+	   count that the project is valid for the current term, is not yet the closed_requests
+	   date and the project have been aproved */
+	public function getSuggestedProjectsGivenCurrentUser($user_id)
+	{	
+		$param[0] = $user_id;
+
+		$sql = 'select spw_project.id, count(project_skills.skill) as nSkillMatch
+		   		  from spw_project, (select skill
+               			   	         from spw_skill_user
+               			   	         where user = ?) as skills, (select spw_project.id, skill
+                                          			             from spw_project, spw_skill_project, spw_term
+                                          			             where (spw_project.id = project) and (spw_project.status = 3) and
+                                                                       (spw_term.id = spw_project.delivery_term) and (spw_term.closed_requests > NOW())) as project_skills
+		   		  where (skills.skill=project_skills.skill) and (spw_project.id=project_skills.id)
+				  group by spw_project.id
+				  order by nSkillMatch DESC';
+
+		$query = $this->db->query($sql, $param);
+
+		$sql1 = 'select count(id)
+				 from spw_project';
+
+	    $query1 = $this->db->query($sql1);
+
+		$totValidProjects = $query1->num_rows();
+
+		$res = $this->chooseRelevantProjects($query, $totValidProjects);
+
+		return $res;
+	}
+
+	public function userHaveProject($user_id)
+	{
+		$param[0] = $user_id;
+
+		$sql = 'select project
+				from spw_user
+				where (id = ?) and (project is not null)';
+
+		$query = $this->db->query($sql, $param);
+
+		return $query;
+	}
+
+	/* given the full list of projects with at least one match, determines which can
+	   actually be suggested to the user*/
+	private function chooseRelevantProjects($allSuggestedProjects, $totalValidProjects)
+	{
+		$count = 0;
+		$ratio = 3;
+		$lSuggestedProjectIds = array();
+		$ratioProjects = round($totalValidProjects / $ratio);
+
+		foreach ($allSuggestedProjects->result() as $row)
+		{
+			if (($row->nSkillMatch >= 2) && ($count < $ratioProjects))
+			{
+				$lSuggestedProjectIds[$count] = $row->id;
+				$count++;
+			}
+		}
+
+		return $lSuggestedProjectIds;
+	}
+
 }
 	
 ?>
