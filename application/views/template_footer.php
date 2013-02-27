d0e713f6215e05f2f4cc849b2407595a9340dc8d
Camilo Sanchez <camilin87@gmail.com>
2013-02-26 19:53:01 -0500

    jquery removed from the project

--- application/views/template_footer.php
+++ application/views/template_footer.php
@@ -10,7 +10,10 @@
 	</div>
 </div>
 
+<!-- this belongs here. However, I don't have time to test it -->
+<!--
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 	<script src="<?php echo base_url() ?>js/bootstrap.min.js"></script>
+-->
 </body>
 </html>
