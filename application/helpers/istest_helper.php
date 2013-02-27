2233d5ac809ca41ffd0a6e8df20b1751b40217d2
Camilo Sanchez <camilin87@gmail.com>
2013-02-26 20:10:40 -0500

    is test helper created

---
+++ application/helpers/istest_helper.php
@@ -0,0 +1,18 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+if ( !function_exists('is_test'))
+{
+	function is_test($sender_controller)
+	{
+		if ($sender_controller->config->item('IS_TEST'))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+}
+
+?>
