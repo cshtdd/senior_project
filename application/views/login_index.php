b6aedfa6f4e1f2fceb70a4326567083c96ecbd9f
Camilo Sanchez <camilin87@gmail.com>
2013-03-09 17:00:57 -0500

    login view completed

--- application/views/login_index.php
+++ application/views/login_index.php
@@ -1,77 +1,86 @@
 <?php $this->load->view("template_header"); ?>
 <h2>Log In</h2>
 
-Do you already have an account on one of these sites? 
+<p>
+	Do you already have an account on one of these sites? 
+</p>
 
-<div class="row-fluid">
+<div class="indent">
+	<div class="row-fluid">
 
-	<div class="span2 well">
-		<a href="<?php echo base_url('/login/oauth2')?>">
-			<img src="https://ssl.gstatic.com/images/logos/google_logo_41.png" />
-		</a>
+		<div class="span2 well">
+			<a href="<?php echo base_url('/login/oauth2')?>">
+				<img src="https://ssl.gstatic.com/images/logos/google_logo_41.png" />
+			</a>
+		</div>
+
+		<div class="span2 well">
+			<img src="https://www.google.com/a/cpanel/fiu.edu/images/logo.gif?service=mail" />
+		</div>
+
+		<div class="span6">
+			<?php echo form_open('admin', array('class' => 'form-signin')) ?>
+
+			<h3>Senior Project Log In</h3>
+
+			<?php 
+
+				//echo form_input('email_address',set_value('email_address'),'id="email_address"');
+				echo form_input(array(
+								'id' => 'email_address',
+								'name' => 'email_address',
+								'type' => 'text',
+								'class' => 'input-block-level input-large',
+								'placeholder' => 'Email address'
+							));
+
+				//<input type="password" class="input-block-level" placeholder="Password">
+				//echo form_password('password','','id="password"');
+				echo form_password(array(
+								'id' => 'password',
+								'name' => 'password',
+								'class' => 'input-block-level input-large',
+								'placeholder' => 'Password'
+							));
+
+				//<button class="btn btn-large btn-primary" type="submit">Sign in</button>
+				//echo form_submit('accounts','Log In');
+				echo form_submit(array(
+					'id' => 'accounts',
+					'name' => 'accounts',
+					'type' => 'Submit',
+					'class' => 'btn btn-large btn-primary',
+					'value' => 'Log In'
+				));
+			?>
+			<?php echo form_close() ?>
+		</div>
+
 	</div>
 
-	<div class="span2 well">
-		<img src="https://www.google.com/a/cpanel/fiu.edu/images/logo.gif?service=mail" />
-	</div>
+	<div class="text-error">
+		<?php echo validation_errors(); ?>
+		<?php
 
-	<div class="span6">
-		<?php echo form_open('admin', array('class' => 'form-signin')) ?>
+			if(isset($credentials_error))
+			{
+				 echo $credentials_error; 
+			}
 
-		<h3>Senior Project Log In</h3>
-
-		<?php 
-
-			//echo form_input('email_address',set_value('email_address'),'id="email_address"');
-			echo form_input(array(
-							'id' => 'email_address',
-							'name' => 'email_address',
-							'type' => 'text',
-							'class' => 'input-block-level input-large',
-							'placeholder' => 'Email address'
-						));
-
-			//<input type="password" class="input-block-level" placeholder="Password">
-			//echo form_password('password','','id="password"');
-			echo form_password(array(
-							'id' => 'password',
-							'name' => 'password',
-							'class' => 'input-block-level input-large',
-							'placeholder' => 'Password'
-						));
-
-			//<button class="btn btn-large btn-primary" type="submit">Sign in</button>
-			//echo form_submit('accounts','Log In');
-			echo form_submit(array(
-				'id' => 'accounts',
-				'name' => 'accounts',
-				'type' => 'Submit',
-				'class' => 'btn btn-large btn-primary',
-				'value' => 'Log In'
-			));
 		?>
-		<?php echo form_close() ?>
 	</div>
-
-</div>
-
-<div class="text-error">
-	<?php echo validation_errors(); ?>
-	<?php
-
-		if(isset($credentials_error))
-		{
-			 echo $credentials_error; 
-		}
-
-	?>
 </div>
 
 
+<p>
+	If you don’t already have an account on any of the above
+</p>
 
-If you don’t already have an account on any of the above
-
-<?php echo anchor('register/', 'click here to sign up', 'style="color: #0077CC; cursor:pointer; text-decoration:none"'); ?>    
+<div class="indent">
+	<h3>
+		<?php echo anchor('register/', 'click here to sign up') ?>    
+	</h3>
+</div>
 
 
 <script type="text/javascript"> 
