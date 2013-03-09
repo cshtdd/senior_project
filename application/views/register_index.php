0952b89ac85f2d762278e5baefcbed5f6ce052d5
Camilo Sanchez <camilin87@gmail.com>
2013-03-09 17:26:33 -0500

    register view stripped OFF unnecessary comments

--- application/views/register_index.php
+++ application/views/register_index.php
@@ -54,49 +54,6 @@
 
 echo form_close() ?>
 
-<!--
-<?php
-	$attributes = array(
-			'id' => 'registration_form'
-	); 
-	echo form_open('register/register', $attributes)
-?>
-<p>
-	<?php 
-
-	  echo form_label('Email Address:','email_address'); 
-	  echo form_input('email_address',set_value('email_address'),'id="email_address"');
-
-	?>
-	<?php 
-
-	  echo form_label('Password:','password_1'); 
-	  echo form_password('password_1','','id="password_1"');
-
-	?>
-
-	<?php 
-
-	  echo form_label('Password:','password_2'); 
-	  echo form_password('password_2','','id="password_2"');
-
-	?>
-</p>
-<p>
-	<?php 
-
-	$data = array(
-	'id' 		 => 'btn',
-    'class'      => 'btn',
-    'name'       => 'accounts',
-    );
-
-	 echo form_submit($data,'Create Senior Project Account');
-	?>
-</p>
-<?php echo form_close() ?>
--->
-
 <div class="errors">
 <?php echo validation_errors(); ?>
 <?php
