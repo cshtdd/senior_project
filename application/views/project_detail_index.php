9540dec8fb8f1f54853720038262f5400e6e2fd6
Camilo Sanchez <camilin87@gmail.com>
2013-03-09 23:28:26 -0500

    project_detail_index code refactored for readability

--- application/views/project_detail_index.php
+++ application/views/project_detail_index.php
@@ -2,13 +2,19 @@
 
 <?php if ($no_results) { ?>
 
-	<p><?php echo $message?></p>
+	<p><?php echo $message ?></p>
 
 <?php } else { ?>
 
 	<?php if (isset($lProjects) && count($lProjects) > 0) { ?>
-		<?php $this->load->view('project_detail_list', array('lProjects' => $lProjects, 'list_title' => $title,
-		                        'lAlikeStudents' => $lAlikeStudents, 'lAlikeMentors' => $lAlikeMentors, 'lEditable' => $lEditable) ) ?>
+		<?php $this->load->view('project_detail_list', 
+			array('lProjects' => $lProjects, 
+				'list_title' => $title,
+				'lAlikeStudents' => $lAlikeStudents, 
+				'lAlikeMentors' => $lAlikeMentors, 
+				'lEditable' => $lEditable
+				) 
+			) ?>
 		<hr>
 	<?php } ?>
 
