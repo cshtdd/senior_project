4b29244aa0827bc5243537d5ffafa1582b95f39e
Camilo Sanchez <camilin87@gmail.com>
2013-03-10 14:17:50 -0400

    spw_project_details_view_model inherits from the right class

--- application/models/spw_project_details_view_model.php
+++ application/models/spw_project_details_view_model.php
@@ -1,6 +1,6 @@
 <?php
 
-class SPW_Project_Details_View_Model extends CI_Model
+class SPW_Project_Details_View_Model extends SPW_Project_Summary_View_Model
 {
 	//so far we have the same fields, so we'll keep it like this
 
