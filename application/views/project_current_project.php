b70bdbbdc23d63440a2698dc2c0a7478d3323566
Camilo Sanchez <camilin87@gmail.com>
2013-03-09 23:41:41 -0500

    project details and current_project views created

---
+++ application/views/project_current_project.php
@@ -0,0 +1,5 @@
+<?php $this->load->view("template_header"); ?>
+
+displays the list of links to the projects of the current user
+
+<?php $this->load->view("template_footer"); ?>
---
+++ application/views/project_details2.php
@@ -0,0 +1,5 @@
+<?php $this->load->view("template_header"); ?>
+
+single project details, with edit possibilities if current user's project
+
+<?php $this->load->view("template_footer"); ?>
