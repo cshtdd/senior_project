95acfb85f82aae94df089c665bc7e34d05efdf1c
Camilo Sanchez <camilin87@gmail.com>
2013-03-10 13:58:25 -0400

    SPW_Project_Details_View_Model class created

---
+++ application/models/spw_project_details_view_model.php
@@ -0,0 +1,13 @@
+<?php
+
+class SPW_Project_Details_View_Model extends CI_Model
+{
+	//so far we have the same fields, so we'll keep it like this
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+}
+
+?>
