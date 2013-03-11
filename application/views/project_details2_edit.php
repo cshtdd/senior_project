f5dcb6947b3302346447f5123ce6f9818bc07dd4
Camilo Sanchez <camilin87@gmail.com>
2013-03-11 12:09:57 -0400

    text area to edit the form

--- application/views/project_details2_edit.php
+++ application/views/project_details2_edit.php
@@ -11,10 +11,65 @@ edit the current project
 -->
 
 
-
-<form>
+<!--
+<form action='/projectcontroller/update'>
 	 <input type="text" name="tags" placeholder="Tags" class="tagManager"/>
+
+	 <input type="submit" value="Save Changes"/>
 </form>
+-->
+
+<?php 
+	echo form_open('projectcontroller/update', array(
+		//'class' => 'form-register',
+		'id' => 'edit_project_form'
+	));
+?>
+	<div class="row-fluid">
+
+		<div class="span9 offset3">
+			<div class="pull-right">
+				<!--<input type="text" name="tags" placeholder="Tags" class="tagManager"/> -->
+				<?php 
+					echo form_input(array(
+						'id' => 'text-new-tag',
+						'name' => 'text-new-tag',
+						'type' => 'text',
+						'class' => 'tagManager input-small',
+						'placeholder' => 'Enter skills...',
+					));
+				?>
+			</div>
+		</div>
+
+		<div class="span12">
+			<?php 
+				echo form_textarea(array(
+					'id' => 'text-description',
+					'name' => 'text-description',
+					//'class' => 'input-large',
+					'rows' => '20',
+					'placeholder' => 'Enter a description for the project...',
+					'value' => $projectDetails->project->description
+				));
+			?>
+		</div>
+
+		<?php 
+			echo form_submit(array(
+				'id' => 'btn-submit',
+				'name' => 'btn-submit',
+				'type' => 'Submit',
+				'class' => 'btn btn-large btn-primary',
+				'value' => 'Save Changes'
+			));
+		?>
+
+	</div>
+<?php
+	echo form_close();
+?>
+
 
 <script type="text/javascript">
 	$(document).ready(function(){
