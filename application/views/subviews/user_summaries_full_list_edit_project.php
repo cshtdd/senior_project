97fb90b755282370ea5986dba548887e570b5ea6
Camilo Sanchez <camilin87@gmail.com>
2013-03-11 13:21:30 -0400

    remove user button added

--- application/views/subviews/user_summaries_full_list_edit_project.php
+++ application/views/subviews/user_summaries_full_list_edit_project.php
@@ -7,20 +7,35 @@
 	}
 ?>
 
-<ul class="inline">
+<ul class="inline" id="<?php echo $prefix.'-container' ?>">
 	<?php 
 		if (isset($lUserSummaries) && count($lUserSummaries) > 0)
 		{
-			foreach ($lUserSummaries as $iUserSummary) 
+			foreach ($lUserSummaries as $user_summary) 
 			{
 		?>
-				<li> 
-					<?php 
-						$this->load->view('subviews/user_summary_full_name_image_edit_project', array(
-							'user_summary' => $iUserSummary
-							) 
-						) 
-					?> 
+				<li id="<?php echo $prefix.'-item-'.$user_summary->user->id ?>"> 
+
+					<a href="#" class="myUserRemover" 
+						id="<?php echo $prefix.'-btn-rem-'.$user_summary->user->id ?>" 
+						tagidtoremove="<?php echo $prefix.'-item-'.$user_summary->user->id ?>" 
+						title="Remove">x</a>
+
+					<div class='user-summary-full center-text'>
+						<?php 
+							$imgUrl = 'http://www.gravatar.com/avatar/';
+							if (isset($user_summary->user->picture) &&
+								strlen($user_summary->user->picture) > 0)
+							{
+								$imgUrl = $user_summary->user->picture;
+							}
+
+							echo img(array(
+								'src' => $imgUrl
+							));
+						?>
+						<?php echo anchor('user/'.$user_summary->user->id, $user_summary->getFullName()) ?>
+					</div>
 				</li>
 		<?php	
 			}
