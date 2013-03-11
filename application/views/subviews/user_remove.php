cc15cb7faac5de084318fad66f269b07fb302cd5
Camilo Sanchez <camilin87@gmail.com>
2013-03-11 17:58:08 -0400

    unnecessary comments removed from user_remove

--- application/views/subviews/user_remove.php
+++ application/views/subviews/user_remove.php
@@ -1,9 +1,3 @@
-<!--
-<a href="#" class="myUserRemover pull-right" 
-	id="<?php echo $prefix.'-btn-rem-'.$user_summary->user->id ?>" 
-	data-idtoremove="<?php echo $prefix.'-item-'.$user_summary->user->id ?>" 
-	title="Remove">x</a>
--->
 <a href="#" class="myUserRemover pull-right" 
 	data-idtoremove="<?php echo $prefix.'-item-'.$user_summary->user->id ?>" 
 	title="Remove">x</a>
