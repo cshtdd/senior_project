4aa9f7258eea623809d6930305dd140b50c7ac86
Camilo Sanchez <camilin87@gmail.com>
2013-03-11 17:15:17 -0400

    user_remove view created

---
+++ application/views/subviews/user_remove.php
@@ -0,0 +1,5 @@
+<a href="#" class="myUserRemover pull-right" 
+	id="<?php echo $prefix.'-btn-rem-'.$user_summary->user->id ?>" 
+	data-idtoremove="<?php echo $prefix.'-item-'.$user_summary->user->id ?>" 
+	title="Remove">x</a>
+<span class="clearfix"></span>
