693b3ca499bbe7d821d09751fbd568c85d7dd642
Camilo Sanchez <camilin87@gmail.com>
2013-03-11 13:39:05 -0400

    remove user links styled

--- application/views/subviews/user_summaries_full_list_edit_project.php
+++ application/views/subviews/user_summaries_full_list_edit_project.php
@@ -20,7 +20,7 @@
 	?>
 				<li id="<?php echo $prefix.'-item-'.$user_summary->user->id ?>"> 
 
-					<a href="#" class="myUserRemover" 
+					<a href="#" class="myUserRemover pull-right" 
 						id="<?php echo $prefix.'-btn-rem-'.$user_summary->user->id ?>" 
 						data-idtoremove="<?php echo $prefix.'-item-'.$user_summary->user->id ?>" 
 						title="Remove">x</a>
