9ad96482dcee3e9971d71710f980e5cb6b5767d5
Camilo Sanchez <camilin87@gmail.com>
2013-03-11 16:39:43 -0400

    the user name and the image are only displayed in user_summary_full_name_image

--- application/views/subviews/user_summaries_full_list_edit_project.php
+++ application/views/subviews/user_summaries_full_list_edit_project.php
@@ -26,22 +26,7 @@
 						data-idtoremove="<?php echo $prefix.'-item-'.$user_summary->user->id ?>" 
 						title="Remove">x</a>
 
-					<div class='user-summary-full center-text'>
-						<?php 
-							$imgUrl = 'http://www.gravatar.com/avatar/';
-							if (isset($user_summary->user->picture) &&
-								strlen($user_summary->user->picture) > 0)
-							{
-								$imgUrl = $user_summary->user->picture;
-							}
-
-							echo img(array(
-								'src' => $imgUrl,
-								'class' => 'user-img'
-							));
-						?>
-						<?php echo anchor('user/'.$user_summary->user->id, $user_summary->getFullName()) ?>
-					</div>
+					<?php $this->load->view('subviews/user_summary_full_name_image', array('user_summary' => $user_summary)) ?>
 				</li>
 	<?php	
 			}
