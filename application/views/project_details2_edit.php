c02c9f27d31a15e16ed8ad3a91c1480719d6d1e4
Camilo Sanchez <camilin87@gmail.com>
2013-03-11 09:21:34 -0400

    raw tag edition on project

--- application/views/project_details2_edit.php
+++ application/views/project_details2_edit.php
@@ -2,9 +2,35 @@
 
 edit the current project
 
+<!--
 <?php
 	print_r($projectDetails); 
 	print_r($suggested_users); 
 ?>
+-->
+
+<form>
+	 <input type="text" name="tags" placeholder="Tags" class="tagManager"/>
+</form>
+
+<script type="text/javascript">
+	$(document).ready(function(){
+
+		jQuery(".tagManager").tagsManager({
+			prefilled: ["Pisa", "Rome"],
+			CapitalizeFirstLetter: true,
+			preventSubmitOnEnter: true,
+			typeahead: true,
+			//typeaheadAjaxSource: null,
+			typeaheadSource: ["Pisa", "Rome", "Milan", "Florence", "New York", "Paris", "Berlin", "London", "Madrid"]
+			//delimeters: [44, 188, 13],
+			//backspace: [8],
+			//blinkBGColor_1: '#FFFF9C',
+			//blinkBGColor_2: '#CDE69C',
+			//hiddenTagListName: 'hiddenTagList'
+		});
+
+	});
+</script>
 
 <?php $this->load->view("template_footer"); ?>
