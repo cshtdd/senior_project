b363867a79f94c2929d0cd45e585c7c64bd381e9
Camilo Sanchez <camilin87@gmail.com>
2013-03-11 16:41:39 -0400

    remove user is on top of the image

--- application/views/subviews/user_summaries_full_list_edit_project.php
+++ application/views/subviews/user_summaries_full_list_edit_project.php
@@ -25,6 +25,7 @@
 						id="<?php echo $prefix.'-btn-rem-'.$user_summary->user->id ?>" 
 						data-idtoremove="<?php echo $prefix.'-item-'.$user_summary->user->id ?>" 
 						title="Remove">x</a>
+					<span class="clearfix"></span>
 
 					<?php $this->load->view('subviews/user_summary_full_name_image', array('user_summary' => $user_summary)) ?>
 				</li>
