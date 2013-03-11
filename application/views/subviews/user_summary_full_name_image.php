c223b2f537c2e35d5740282ff0996e507ff50694
Camilo Sanchez <camilin87@gmail.com>
2013-03-10 22:59:28 -0400

    user image and name appear centered

--- application/views/subviews/user_summary_full_name_image.php
+++ application/views/subviews/user_summary_full_name_image.php
@@ -1,11 +1,15 @@
-<?php 
-	$imgUrl = 'http://www.gravatar.com/avatar/';
-	if (isset($user_summary->user->picture) &&
-		strlen($user_summary->user->picture) > 0)
-	{
-		$imgUrl = $user_summary->user->picture;
-	}
+<div class='user-summary-full center-text'>
+	<?php 
+		$imgUrl = 'http://www.gravatar.com/avatar/';
+		if (isset($user_summary->user->picture) &&
+			strlen($user_summary->user->picture) > 0)
+		{
+			$imgUrl = $user_summary->user->picture;
+		}
 
-	echo img($imgUrl);
-?>
-<?php echo anchor('user/'.$user_summary->user->id, $user_summary->getFullName()) ?>
+		echo img(array(
+			'src' => $imgUrl
+		));
+	?>
+	<?php echo anchor('user/'.$user_summary->user->id, $user_summary->getFullName()) ?>
+</div>
