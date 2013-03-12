101db09883948ed9bc03da15b32fc375040695c4
Camilo Sanchez <camilin87@gmail.com>
2013-03-12 12:26:10 -0400

    user summary with image has a link on the image

--- application/views/subviews/user_summary_full_name_image.php
+++ application/views/subviews/user_summary_full_name_image.php
@@ -7,10 +7,14 @@
 			$imgUrl = $user_summary->user->picture;
 		}
 
-		echo img(array(
-			'src' => $imgUrl,
-			'class' => 'user-img'
-		));
+		echo '<a href="'.base_url().'user/'.$user_summary->user->id.'">';
+
+			echo img(array(
+				'src' => $imgUrl,
+				'class' => 'user-img'
+			));
+
+		echo '</a>';
 	?>
 	<?php echo anchor('user/'.$user_summary->user->id, $user_summary->getFullName()) ?>
 </div>
