04a6c44ed19f94b73ce17b7268392fdcfeed074f
Camilo Sanchez <camilin87@gmail.com>
2013-03-12 17:18:41 -0400

    form to update user profile submit button created

--- application/views/user_profile_edit.php
+++ application/views/user_profile_edit.php
@@ -13,84 +13,103 @@
 	{
 ?>
 
+		<?php echo form_open('usercontroller/update', array(
+			'id' => 'form-update-user'
+		)); ?>
 
-		<?php echo anchor('/user/linkedIn_initiate', 'Sync with LinkedIn', array('class' => 'btn btn-primary pull-right'))  ?>
+			<div>
 
-		<div class="row-fluid">
-			<div class="span4 center-text">
-				<?php 
-					echo img(array(
-						'src' => $userDetails->user->picture,
-						'class' => 'user-img-large'
-					))
-				?>
-			</div>
+				<?php echo anchor('/user/linkedIn_initiate', 'Sync with LinkedIn', array('class' => 'btn btn-primary pull-right'))  ?>
 
-			<div class="span8">
-				<h3>
-					<?php echo $userDetails->getFullName() ?>
-				</h3>
+				<div class="row-fluid">
+					<div class="span4 center-text">
+						<?php 
+							echo img(array(
+								'src' => $userDetails->user->picture,
+								'class' => 'user-img-large'
+							))
+						?>
+					</div>
 
-				<?php echo $userDetails->role->name ?>
+					<div class="span8">
+						<h3>
+							<?php echo $userDetails->getFullName() ?>
+						</h3>
 
-				<?php 
-					if (isset($userDetails->user->graduation_term) &&
-						isset($userDetails->user->graduation_term->name) && 
-						strlen($userDetails->user->graduation_term->name) > 0) {
-				?>
-					Graduating In
-					<?php echo $userDetails->user->graduation_term->name ?>
-				<?php } ?>
-			</div>
-		</div>
+						<?php echo $userDetails->role->name ?>
+
+						<?php 
+							if (isset($userDetails->user->graduation_term) &&
+								isset($userDetails->user->graduation_term->name) && 
+								strlen($userDetails->user->graduation_term->name) > 0) {
+						?>
+							Graduating In
+							<?php echo $userDetails->user->graduation_term->name ?>
+						<?php } ?>
+					</div>
+				</div>
 
 
-		<div class="spaced-top">
-			<?php if (isset($userDetails->lSkills) && count($userDetails->lSkills) > 0) { ?>
-				<h4>Skills</h4>
-				<?php $this->load->view('subviews/skills_list', array('lSkills' => $userDetails->lSkills) )?>
-			<?php }?>
-		</div>
+				<div class="spaced-top">
+					<?php if (isset($userDetails->lSkills) && count($userDetails->lSkills) > 0) { ?>
+						<h4>Skills</h4>
+						<?php $this->load->view('subviews/skills_list', array('lSkills' => $userDetails->lSkills) )?>
+					<?php }?>
+				</div>
 
-		<div class="spaced-top">
-			<?php if (isset($userDetails->lLanguages) && count($userDetails->lLanguages) > 0) { ?>
-				<h4>Languages</h4>
-				<?php $this->load->view('subviews/skills_list', array('lSkills' => $userDetails->lLanguages) )?>
-			<?php }?>
-		</div>
+				<div class="spaced-top">
+					<?php if (isset($userDetails->lLanguages) && count($userDetails->lLanguages) > 0) { ?>
+						<h4>Languages</h4>
+						<?php $this->load->view('subviews/skills_list', array('lSkills' => $userDetails->lLanguages) )?>
+					<?php }?>
+				</div>
 
-		<div class="spaced-top">
-			<?php if(isset($userDetails->user->summary_spw) && strlen($userDetails->user->summary_spw) > 0) {?>
-				<h4>Short Bio</h4>
-				<?php echo $userDetails->user->summary_spw ?>
-			<?php }?>
-		</div>
+				<div class="spaced-top">
+					<?php if(isset($userDetails->user->summary_spw) && strlen($userDetails->user->summary_spw) > 0) {?>
+						<h4>Short Bio</h4>
+						<?php echo $userDetails->user->summary_spw ?>
+					<?php }?>
+				</div>
 
-		<div class="spaced-top">
-			<?php if(isset($userDetails->user->summary_linkedIn) && strlen($userDetails->user->summary_linkedIn) > 0) {?>
-				<h4>Linked In Summary</h4>
-				<?php echo $userDetails->user->summary_linkedIn ?>
-			<?php }?>
-		</div>
+				<div class="spaced-top">
+					<?php if(isset($userDetails->user->summary_linkedIn) && strlen($userDetails->user->summary_linkedIn) > 0) {?>
+						<h4>Linked In Summary</h4>
+						<?php echo $userDetails->user->summary_linkedIn ?>
+					<?php }?>
+				</div>
 
-		<div class="spaced-top">
-			<?php if (isset($userDetails->lExperiences) && count($userDetails->lExperiences) > 0) {	?>
-				<h4>Experience</h4>
-				<ul>
-					<?php foreach($userDetails->lExperiences as $iExperience) { ?>
-						<li class="well">
-							<h5>
-								<?php echo $iExperience->title ?>
-							</h5>
+				<div class="spaced-top">
+					<?php if (isset($userDetails->lExperiences) && count($userDetails->lExperiences) > 0) {	?>
+						<h4>Experience</h4>
+						<ul>
+							<?php foreach($userDetails->lExperiences as $iExperience) { ?>
+								<li class="well">
+									<h5>
+										<?php echo $iExperience->title ?>
+									</h5>
 
-							<p>
-								<?php echo $iExperience->description ?>
-							</p>
-						</li>
+									<p>
+										<?php echo $iExperience->description ?>
+									</p>
+								</li>
+							<?php } ?>
+						</ul>
 					<?php } ?>
-				</ul>
-			<?php } ?>
-		</div>
+				</div>
+
+				<?php 
+					echo form_submit(array(
+						'id' => 'btn-submit',
+						'name' => 'btn-submit',
+						'type' => 'Submit',
+						'class' => 'btn btn-large btn-primary pull-right',
+						'value' => 'Save Changes'
+					));
+				?>
+
+			</div>
+
+		<?php echo form_close() ?>
 <?php 
 	}
 ?>
