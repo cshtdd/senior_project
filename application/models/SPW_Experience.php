f33b3f4734e8f8e4fce4c361021a02fb11b605f4
Camilo Sanchez <camilin87@gmail.com>
2013-03-12 12:59:11 -0400

    spw_experience model created

---
+++ application/models/SPW_Experience.php
@@ -0,0 +1,15 @@
+<?php
+
+class SPW_Experience extends CI_Model
+{
+	public $id;
+	public $title;
+	public $description;
+
+	public function __construct()
+	{
+		parent::__construct();
+	}
+}
+	
+?>
