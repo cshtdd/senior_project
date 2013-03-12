7bf54f753f0d6643c4b247c70eaf652be37d32cb
Camilo Sanchez <camilin87@gmail.com>
2013-03-12 14:07:34 -0400

    proper heading for elements

--- application/views/user_profile.php
+++ application/views/user_profile.php
@@ -1,5 +1,7 @@
 <?php $this->load->view("template_header"); ?>
 
+	<h2>User Details</h2>
+
 <?php 
 	if ($no_results) 
 	{
@@ -13,48 +15,53 @@
 
 		<?php echo img(array('src' => $userDetails->user->picture)) ?>
 
-		<?php echo $userDetails->getFullName() ?>
+		<h3>
+			<?php echo $userDetails->getFullName() ?>
+		</h3>
 
 		<?php echo $userDetails->role->name ?>
 
 		<?php echo $userDetails->term->description ?>
 
+		<div>
+			<?php if (isset($userDetails->lSkills) && count($userDetails->lSkills) > 0) { ?>
+				<h4>Skills</h4>
+				<?php $this->load->view('subviews/skills_list', array('lSkills' => $userDetails->lSkills) )?>
+			<?php }?>
+		</div>
 
-		<?php if (isset($userDetails->lSkills) && count($userDetails->lSkills) > 0) { ?>
-			Skills
-			<?php $this->load->view('subviews/skills_list', array('lSkills' => $userDetails->lSkills) )?>
-		<?php }?>
+		<div>
+			<?php if (isset($userDetails->lLanguages) && count($userDetails->lLanguages) > 0) { ?>
+				<h4>Languages</h4>
+				<?php $this->load->view('subviews/skills_list', array('lSkills' => $userDetails->lLanguages) )?>
+			<?php }?>
+		</div>
 
+		<div>
+			<?php if(isset($userDetails->user->summary_spw) && strlen($userDetails->user->summary_spw) > 0) {?>
+				<h4>Short Bio</h4>
+				<?php echo $userDetails->user->summary_spw ?>
+			<?php }?>
+		</div>
 
-		<?php if (isset($userDetails->lLanguages) && count($userDetails->lLanguages) > 0) { ?>
-			Languages
-			<?php $this->load->view('subviews/skills_list', array('lSkills' => $userDetails->lLanguages) )?>
-		<?php }?>
+		<div>
+			<?php if (isset($userDetails->lExperiences) && count($userDetails->lExperiences) > 0) {	?>
+				<h4>Experiences</h4>
+				<ul>
+					<?php foreach($userDetails->lExperiences as $iExperience) { ?>
+						<li>
+							<p>
+								<?php echo $iExperience->title ?>
+							</p>
 
-
-		<?php if(isset($userDetails->user->summary_spw) && strlen($userDetails->user->summary_spw) > 0) {?>
-			Short Bio
-			<?php echo $userDetails->user->summary_spw ?>
-		<?php }?>
-
-
-		<?php if (isset($userDetails->lExperiences) && count($userDetails->lExperiences) > 0) {	?>
-			<ul>
-				<lh>Experiences</lh>
-				<?php foreach($userDetails->lExperiences as $iExperience) { ?>
-					<li>
-						<p>
-							<?php echo $iExperience->title ?>
-						</p>
-
-						<p>
-							<?php echo $iExperience->description ?>
-						</p>
-					</li>
-				<?php } ?>
-			</ul>
-		<?php } ?>
-
+							<p>
+								<?php echo $iExperience->description ?>
+							</p>
+						</li>
+					<?php } ?>
+				</ul>
+			<?php } ?>
+		</div>
 <?php 
 	}
 ?>
