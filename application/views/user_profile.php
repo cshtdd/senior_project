4933fba568b5792a3c3f7f479b44e34e5aa23f32
Camilo Sanchez <camilin87@gmail.com>
2013-03-12 13:58:23 -0400

    very ugly user details view with all the data to be presented

--- application/views/user_profile.php
+++ application/views/user_profile.php
@@ -1,5 +1,51 @@
 <?php $this->load->view("template_header"); ?>
 
-user profile 
+<?php 
+	if ($no_results) 
+	{
+?>
+		<p>No data for the specified user</p>
+<?php 
+	} 
+	else 
+	{
+?>
+
+		<?php echo img(array('src' => $userDetails->user->picture)) ?>
+
+		<?php echo $userDetails->getFullName() ?>
+
+		<?php echo $userDetails->role->name ?>
+
+		<?php echo $userDetails->term->description ?>
+
+		<?php $this->load->view('subviews/skills_list', array('lSkills' => $userDetails->lSkills) )?>
+
+		<?php echo $userDetails->user->summary_spw ?>
+
+		<?php $this->load->view('subviews/skills_list', array('lSkills' => $userDetails->lLanguages) )?>
+
+
+
+		<?php if (isset($userDetails->lExperiences) && count($userDetails->lExperiences) > 0) {	?>
+			<ul>
+				<lh>Experiences</lh>
+				<?php foreach($userDetails->lExperiences as $iExperience) { ?>
+					<li>
+						<p>
+							<?php echo $iExperience->title ?>
+						</p>
+
+						<p>
+							<?php echo $iExperience->description ?>
+						</p>
+					</li>
+				<?php } ?>
+			</ul>
+		<?php } ?>
+
+<?php 
+	}
+?>
 
 <?php $this->load->view("template_footer"); ?>
