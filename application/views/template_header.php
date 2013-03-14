bcddc0b25c13cb5c9a49f1f0b1b35e58f4960d6a
Camilo Sanchez <camilin87@gmail.com>
2013-03-14 16:45:48 -0400

    new navigation bar beginning to take form

--- application/views/template_header.php
+++ application/views/template_header.php
@@ -34,136 +34,86 @@
 <body>
     <div class="container-narrow">
 
-        <?php if ( !stristr(uri_string(), 'login') ) { ?>
 
+        <div class="navbar">
+          <div class="navbar-inner">
+            <?php echo anchor('/', 'FIU Senior Project', array('class' => 'brand')) ?>
 
-            <!-- very nice html, but apparently the form-helper is more secure--> 
-            <!--
-                    <form class="search-form form-inline" action="<?php echo site_url('searchcontroller/search_string') ?>">
-                        <div class="input-append">
-                            <input id="text-search-top" name="q" type="text" class="span2" placeholder="just search...">
-                            <button type="submit" class="btn" >Search</button>
-                        </div>
-                    </form>
-            -->
+            <?php if ( !stristr(uri_string(), 'login') ) { ?>
 
-            <?php echo form_open('searchcontroller/search_string', array(
-                'class' => 'search-form form-inline', 
-                'id' => 'search-form-top',
-                'method' => 'GET')) ?>
-                <div class="input-append">
-                    <?php 
-                        echo form_input(array(
-                            'id' => 'text-search-top',
-                            'name' => 'q',
-                            'type' => 'text',
-                            'class' => 'span4',
-                            'placeholder' => 'search for people, skills, projects and terms...',
-                            'required' => '',
-                            'title' => 'search criteria'
-                        ));
-
-                        echo form_button(array(
-                            'id' => 'btn-search-top',
-                            'type' => 'Submit',
-                            'class' => 'btn',
-                            'content' => 'Search'
-                        ));
-
-                    ?>
-                </div>
-            <?php echo form_close() ?>
-
-            <?php
-                function get_nav_item_class($targetUrl)
-                {
-                    $li_class_str = '';
-
-                    if( trim(strtolower(uri_string())) == trim(strtolower($targetUrl)) )
+                <?php
+                    function get_nav_item_class($targetUrl)
                     {
-                        $li_class_str = 'class="active"';
-                    }
+                        $li_class_str = '';
 
-                    return $li_class_str;
-                }
-                function get_nav_item_Internal($targetUrl, $innerHTML)
-                {
-                    return '<li '.get_nav_item_class($targetUrl).'>'.$innerHTML.'</li>';
-                }
-                function get_nav_item($targetUrl, $displayText)
-                {
-                    return get_nav_item_Internal($targetUrl, anchor($targetUrl, $displayText));
-                }
-            ?>
-
-            <ul class="nav nav-pills pull-right">
-                <?php echo get_nav_item('home', 'Home') ?>
-                <?php echo get_nav_item('past-projects', 'Past Projects') ?>
-                <?php echo get_nav_item('project', 'My Projects') ?>
-                <?php echo get_nav_item('about', 'About') ?>
-            <!--    
-                <?php echo get_nav_item('me', 'My Profile') ?>            
-                <?php echo get_nav_item('logout', 'Logout') ?>
-            -->
-
-                <?php if (isUserLoggedIn($this)) { ?>
-                    <?php echo get_nav_item('me', 'My Profile') ?>            
-                    <?php echo get_nav_item('logout', 'Logout') ?>
-    <!--
-                    <?php 
-                        //echo get_nav_item('me', getCurrentUserHeaderName($this)) 
-
-                        $linkText = getCurrentUserHeaderName($this);
-
-                        $imgSrc = getCurrentUserHeaderImg($this);
-                        $imgHtml = '';
-
-                        if (strlen($imgSrc) > 0)
+                        if( trim(strtolower(uri_string())) == trim(strtolower($targetUrl)) )
                         {
-                            //$linkText = $linkText.' '.$imgHtml;
-                            $imgHtml = img(array(
-                                    'src' => $imgSrc,
-                                    'class' => 'img-header-profile',
-                                    'alt' => 'User Profile Image'
-                                ));
+                            $li_class_str = 'class="active"';
                         }
 
-                        //$navItemHTML = '<a href="'.base_url().'me">'.$linkText.$imgHtml.'</a>';
-                        $navItemHTML = anchor('me', $linkText.$imgHtml);
+                        return $li_class_str;
+                    }
+                    function get_nav_item_Internal($targetUrl, $innerHTML)
+                    {
+                        return '<li '.get_nav_item_class($targetUrl).'>'.$innerHTML.'</li>';
+                    }
+                    function get_nav_item($targetUrl, $displayText)
+                    {
+                        return get_nav_item_Internal($targetUrl, anchor($targetUrl, $displayText));
+                    }
+                ?>
 
-                        //echo get_nav_item('me', $linkText);
-                        echo get_nav_item_Internal('me', $navItemHTML);
+                <ul class="nav pull">
+                    <!-- <?php echo get_nav_item('home', 'Home') ?> -->
+                    <?php echo get_nav_item('past-projects', 'Past Projects') ?>
+                    <?php echo get_nav_item('project', 'My Projects') ?>
+                    <?php echo get_nav_item('about', 'About') ?>
+                </ul>
 
-                        //echo $navItemHTML;
-                    ?>
-                    
-                    <?php echo get_nav_item('logout', 'Logout') ?>
-    -->
-                    <!--
-                    <?php
-                         echo get_nav_item('logout', 
-                            img(array(
-                                    'src' => getCurrentUserHeaderImg($this),
-                                    'class' => 'img-header-profile',
-                                    'alt' => 'User Profile Image'
-                                ))
-                            ) 
-                    ?>
-                    -->
+                <ul class="nav pull-right">
+                    <?php if (isUserLoggedIn($this)) { ?>
 
-                <?php } else { ?>
+                        <?php echo get_nav_item('logout', 'Logout') ?> 
 
-                    <?php echo get_nav_item('login', 'Login') ?>
+                    <?php } else { ?>
 
-                <?php } ?>
-            </ul>
-        <?php } ?>
+                        <?php echo get_nav_item('login', 'Login') ?> 
 
+                    <?php } ?>
+                </ul>
 
-        <h1 class="muted">
-            <?php echo anchor('/', 'Senior Project Website') ?>
-        </h1>
+                <?php echo form_open('searchcontroller/search_string', array(
+                    //'class' => 'search-form form-inline navbar-search pull-right', 
+                    'class' => 'navbar-search pull-right', 
+                    'id' => 'search-form-top',
+                    'method' => 'GET')) ?>
+                    <div class="input-append">
+                        <?php 
+                            echo form_input(array(
+                                'id' => 'text-search-top',
+                                'name' => 'q',
+                                'type' => 'text',
+                                'class' => 'span3 search-query',
+                                'placeholder' => 'search for people, skills, projects and terms...',
+                                'required' => '',
+                                'title' => 'search criteria'
+                            ));
 
-        <hr>
+                            /*
+                            echo form_button(array(
+                                'id' => 'btn-search-top',
+                                'type' => 'Submit',
+                                'class' => 'btn',
+                                'content' => 'Search'
+                            ));
+                            */
+
+                        ?>
+                    </div>
+                <?php echo form_close() ?>                
+
+            <?php } ?>
+          </div>
+        </div>
 
         <div id="main-content">
