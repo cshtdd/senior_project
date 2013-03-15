1aad8d5a9665388ab653d5567c9dde1c5f95a5ee
Camilo Sanchez <camilin87@gmail.com>
2013-03-15 15:31:23 -0400

    displaying flash message

--- application/helpers/flash_message_helper.php
+++ application/helpers/flash_message_helper.php
@@ -4,6 +4,11 @@ if ( !function_exists('isFlashMessageAvailable'))
 {
     function isFlashMessageAvailable($sender_controller)
     {
+        if (is_test($sender_controller))
+        {
+            //return true;
+        }
+
         $flashMessage = $sender_controller->session->flashdata('FLASH_MESSAGE');
 
         if (isset($flashMessage) && strlen($flashMessage) > 0)
@@ -21,6 +26,11 @@ if ( !function_exists('getFlashMessage'))
 {
     function getFlashMessage($sender_controller)
     {
+        if (is_test($sender_controller))
+        {
+            //return 'This is a flash message';
+        }
+
         $flashMessage = $sender_controller->session->flashdata('FLASH_MESSAGE');
 
         if (isset($flashMessage) && strlen($flashMessage) > 0)
