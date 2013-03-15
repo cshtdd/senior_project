d7531423032b51ddc1ace06b72f80da1a09f2181
Camilo Sanchez <camilin87@gmail.com>
2013-03-15 10:22:50 -0400

    accept_notification reject_notification hide_notification
    methods added to the notificationscontroller

--- application/controllers/notificationscontroller.php
+++ application/controllers/notificationscontroller.php
@@ -8,12 +8,30 @@ class NotificationsController extends CI_Controller
         parent::__construct();
         //$this->output->cache(60);
     }
-    
-    
+
     public function display_notifications()
     {
         $this->output->set_output('you have notifications');
     }
-    
-    
+
+
+
+    public function accept_notification($notificationId)
+    {
+        //with the notification Id take the appropriate action
+
+        $this->output->set_output('notification accepted');
+    }
+
+    public function reject_notification($notificationId)
+    {
+        //with the notification Id take the appropriate action
+
+        $this->output->set_output('notification rejected');
+    }
+
+    public function hide_notification($notificationId)
+    {
+        $this->output->set_output('notification hidden');
+    }
 }
