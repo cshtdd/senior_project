a40018f96dac548087f9c8db085af4082b6c867b
Keiser Moya <kmoya002@fiu.edu>
2013-03-18 11:09:38 -0400

    some changes overall
    Does not affect anybody

--- application/models/spw_role_model.php
+++ application/models/spw_role_model.php
@@ -21,6 +21,28 @@ class SPW_Role_Model extends CI_Model
 
 		return $query->result();
 	}
+
+	//get all available roles
+	public function getAllRoles()
+	{
+		$sql = 'select *
+				from spw_role
+				where (id <> 1) or (id <> 2)';
+		$query = $this->db->query($sql);
+
+		$roleNum = $query->num_rows();
+		$lRoles = array();
+		if ($roleNum > 0)
+        {
+        	for ($j = 0; $j < $roleNum; $i++)
+        	{
+        		$row = $query->row($j, 'SPW_Role_Model');
+				$lRoles[] = $row;
+        	}
+        }
+
+        return $lRoles;
+	}
 }
 	
 ?>
