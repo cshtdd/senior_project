db6698afb931122f56ae4de0e7933c512b7e3686
Camilo Sanchez <camilin87@gmail.com>
2013-03-22 15:13:52 -0400

    html5shim https://code.google.com/p/html5shim/

--- application/views/template_header.php
+++ application/views/template_header.php
@@ -33,7 +33,9 @@
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
     <script type="text/javascript" src="<?php echo base_url() ?>js/bootstrap.min.js"></script>
     <script type="text/javascript" src="<?php echo base_url() ?>js/bootstrap-tagmanager.js"></script>
-
+	<!--[if lt IE 9]>
+	<script src="<?php echo base_url() ?>js/html5shiv.js"></script>
+	<![endif]-->
 </head>
 <body>
     <div class="container-narrow">
