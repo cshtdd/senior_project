19dc59557808e710de80798a2bbdc5356cdfff3d
Camilo Sanchez <camilin87@gmail.com>
2013-03-21 22:29:53 -0400

    email adde to user details test data

--- application/controllers/usercontroller.php
+++ application/controllers/usercontroller.php
@@ -525,6 +525,7 @@ class UserController extends CI_Controller
         $user1->id = $user_id;
         $user1->first_name = 'Flash';
         $user1->last_name = 'Gordon';
+        $user1->email = 'lolo@gmail.com';
         $user1->picture = 'http://i0.kym-cdn.com/photos/images/newsfeed/000/162/317/2vA1a.png?1313349760';
         $user1->summary_spw = 'Mobile oriented developer. Has worked for the biggest players in the field.';
         $user1->summary_linkedIn = 'Worked as a security expert at LinkedIn right after they lost all of their passwords';
