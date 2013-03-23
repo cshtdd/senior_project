9de5f172b76f26a6d88792186061ff8a0a8441a5
Camilo Sanchez <camilin87@gmail.com>
2013-03-23 11:48:26 -0400

    tabs to spaces

--- application/models/spw_user_summary_view_model.php
+++ application/models/spw_user_summary_view_model.php
@@ -2,58 +2,58 @@
 
 class SPW_User_Summary_View_Model extends CI_Model
 {
-	//a SPW_User_Model object
-	public $user;
+    //a SPW_User_Model object
+    public $user;
 
-	public $invite;
+    public $invite;
 
-	public function __construct()
-	{
-		parent::__construct();
-	}
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-	public function getFullName()
-	{
-		return ucwords($this->user->first_name.' '.$this->user->last_name);
-	}
+    public function getFullName()
+    {
+        return ucwords($this->user->first_name.' '.$this->user->last_name);
+    }
 
-	public function prepareUsersDataToShow($current_user_id, $lUserIds)
-	{
-		$length = count($lUserIds);
+    public function prepareUsersDataToShow($current_user_id, $lUserIds)
+    {
+        $length = count($lUserIds);
 
-		$lUsers = array();
+        $lUsers = array();
 
-		for ($i = 0; $i < $length; $i++)
-		{
-			$user_summ_vm = new SPW_User_Summary_View_Model();
+        for ($i = 0; $i < $length; $i++)
+        {
+            $user_summ_vm = new SPW_User_Summary_View_Model();
 
-			$param[0] = $lUserIds[$i];
+            $param[0] = $lUserIds[$i];
 
-			//get user info
-			$sql = 'select *
-					from spw_user
-					where (id = ?)';
-			$query = $this->db->query($sql, $param);
+            //get user info
+            $sql = 'select *
+                    from spw_user
+                    where (id = ?)';
+            $query = $this->db->query($sql, $param);
 
-			if ($query->num_rows() > 0)
-			{
-				$row = $query->row(0, 'SPW_User_Model');
-				$user = $row;
-				$user_summ_vm->user = $user;
+            if ($query->num_rows() > 0)
+            {
+                $row = $query->row(0, 'SPW_User_Model');
+                $user = $row;
+                $user_summ_vm->user = $user;
 
-				$user_summ_vm->invite = $user->canInviteUser($current_user_id, $lUserIds[$i]);
-			}
-			else
-			{
-				throw new Exception('spw_user table error in db...');
-			}	
+                $user_summ_vm->invite = $user->canInviteUser($current_user_id, $lUserIds[$i]);
+            }
+            else
+            {
+                throw new Exception('spw_user table error in db...');
+            }   
 
-			$lUsers[] = $user_summ_vm;		
-		}
+            $lUsers[] = $user_summ_vm;      
+        }
 
-		return $lUsers;
-	}
+        return $lUsers;
+    }
 
 }
-	
+    
 ?>
