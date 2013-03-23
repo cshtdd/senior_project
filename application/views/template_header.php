958ad584a10265a37abab9ceb4ddbea860c4a192
Camilo Sanchez <camilin87@gmail.com>
2013-03-23 15:44:20 -0400

    navigation bar collapses for small screens

--- application/views/template_header.php
+++ application/views/template_header.php
@@ -43,165 +43,178 @@
 
         <div class="navbar">
           <div class="navbar-inner">
-            <?php echo anchor('/', 'FIU Senior Project', array('class' => 'brand')) ?>
+            <div class="container">
+                <?php echo anchor('/', 'FIU Senior Project', array('class' => 'brand')) ?>
 
-            <?php if ( !in_array(strtolower(uri_string()), array('login', 'register')) ) { ?>            
+                <?php if ( !in_array(strtolower(uri_string()), array('login', 'register')) ) { ?>            
 
-                <?php
-                    function isActiveNavItem($targetUrl)
-                    {
-                        return trim(strtolower(uri_string())) == trim(strtolower($targetUrl));
-                    }
-                    function get_nav_item_class($targetUrl)
-                    {
-                        $li_class_str = '';
-
-                        if( isActiveNavItem($targetUrl) )
-                        {
-                            $li_class_str = 'class="active"';
-                        }
-
-                        return $li_class_str;
-                    }
-                    function get_nav_item_Internal($targetUrl, $innerHTML)
-                    {
-                        return '<li '.get_nav_item_class($targetUrl).'>'.$innerHTML.'</li>';
-                    }
-                    function get_nav_item($targetUrl, $displayText)
-                    {
-                        return get_nav_item_Internal($targetUrl, anchor($targetUrl, $displayText));
-                    }
-                ?>
-
-                <ul class="nav pull">
-                    <!-- <?php echo get_nav_item('home', 'Home') ?> -->
-                    <?php echo get_nav_item('past-projects', 'Past Projects') ?>
-                    <?php echo get_nav_item('project', 'My Projects') ?>
-                    <?php echo get_nav_item('about', 'About') ?>
-                </ul>
-
-                <ul class="nav pull-right">
-
-                    <?php 
-                        $notificationsCount = getPendingNotificationsCount($this);
-                        if ($notificationsCount > 0)
-                        {
-                    ?>
-                            <li id="notifications-badge" <?php echo get_nav_item_class('notifications') ?> >
-                                <a href="<?php echo base_url().'notifications' ?>">
-                                    <i class="icon-envelope"></i>
-                                    <span class="badge badge-important"><?php echo $notificationsCount ?></span>
-                                </a>
-                            </li>
                     <?php
+                        function isActiveNavItem($targetUrl)
+                        {
+                            return trim(strtolower(uri_string())) == trim(strtolower($targetUrl));
+                        }
+                        function get_nav_item_class($targetUrl)
+                        {
+                            $li_class_str = '';
+
+                            if( isActiveNavItem($targetUrl) )
+                            {
+                                $li_class_str = 'class="active"';
+                            }
+
+                            return $li_class_str;
+                        }
+                        function get_nav_item_Internal($targetUrl, $innerHTML)
+                        {
+                            return '<li '.get_nav_item_class($targetUrl).'>'.$innerHTML.'</li>';
+                        }
+                        function get_nav_item($targetUrl, $displayText)
+                        {
+                            return get_nav_item_Internal($targetUrl, anchor($targetUrl, $displayText));
                         }
                     ?>
 
-                    <?php if (isUserLoggedIn($this)) { ?>                      
+                        <!-- hamburger button to be displayed when taskbar is collapsed -->
+                        <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </a>
 
 
-                        <li class="<?php echo isActiveNavItem('me') ? 'dropdown active' : 'dropdown' ?>">
-                            <a id="link-profile" href="<?php echo base_url().'me' ?>" role="button" class="dropdown-toggle" data-toggle="dropdown">
-                                <i class="icon-user"></i> <b class="caret"></b>
-                            
-                            <ul class="dropdown-menu" role="menu" aria-labelledby="link-profile">
-                                <li role="presentation">
-                                    <a role="menuitem" tabindex="-1" href="<?php echo base_url().'me' ?>">
+                        <div class="nav-collapse collapse">
 
-                                        <div>
-                                            <div class="pull-right"> 
-                                                <strong><?php echo getCurrentUserHeaderFullName($this)?></strong>
-                                                <small class="block-text">View Profile</small>                                                
-                                            </div>
-                                            <?php 
-                                                $imgSrc = getUserImage($this, getCurrentUserHeaderImg($this));
-                                                if (isset($imgSrc))
-                                                {
-                                                    echo img(array(
-                                                            'src' => $imgSrc,
-                                                            'alt' => 'User Profile Image',
-                                                            'class' => 'img-header-profile-big img-polaroid'
-                                                        ));
-                                                }
-                                            ?>
-                                        </div>
-                                    </a>                                    
-                                </li>
+                            <ul class="nav pull">
+                                <!-- <?php echo get_nav_item('home', 'Home') ?> -->
+                                <?php echo get_nav_item('past-projects', 'Past Projects') ?>
+                                <?php echo get_nav_item('project', 'My Projects') ?>
+                                <?php echo get_nav_item('about', 'About') ?>
+                            </ul>
+
+                            <ul class="nav pull-right">
 
                                 <?php 
                                     $notificationsCount = getPendingNotificationsCount($this);
                                     if ($notificationsCount > 0)
                                     {
                                 ?>
-                                        <li role="presentation">
-                                            <a role="menuitem" tabindex="-1" href="<?php echo base_url().'notifications' ?>">
-
-                                                
+                                        <li id="notifications-badge" <?php echo get_nav_item_class('notifications') ?> >
+                                            <a href="<?php echo base_url().'notifications' ?>">
+                                                <i class="icon-envelope"></i>
                                                 <span class="badge badge-important"><?php echo $notificationsCount ?></span>
-                                                Notifications Pending                                             
-                                            
-
                                             </a>
                                         </li>
                                 <?php
                                     }
                                 ?>
 
-                                <li role="presentation" class="divider"></li>
-                                <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo base_url().'logout' ?>">Logout</a></li>
+                                <?php if (isUserLoggedIn($this)) { ?>                      
+
+
+                                    <li class="<?php echo isActiveNavItem('me') ? 'dropdown active' : 'dropdown' ?>">
+                                        <a id="link-profile" href="<?php echo base_url().'me' ?>" role="button" class="dropdown-toggle" data-toggle="dropdown">
+                                            <i class="icon-user"></i> <b class="caret"></b>
+                                        
+                                        <ul class="dropdown-menu" role="menu" aria-labelledby="link-profile">
+                                            <li role="presentation">
+                                                <a role="menuitem" tabindex="-1" href="<?php echo base_url().'me' ?>">
+
+                                                    <div>
+                                                        <div class="pull-right"> 
+                                                            <strong><?php echo getCurrentUserHeaderFullName($this)?></strong>
+                                                            <small class="block-text">View Profile</small>                                                
+                                                        </div>
+                                                        <?php 
+                                                            $imgSrc = getUserImage($this, getCurrentUserHeaderImg($this));
+                                                            if (isset($imgSrc))
+                                                            {
+                                                                echo img(array(
+                                                                        'src' => $imgSrc,
+                                                                        'alt' => 'User Profile Image',
+                                                                        'class' => 'img-header-profile-big img-polaroid'
+                                                                    ));
+                                                            }
+                                                        ?>
+                                                    </div>
+                                                </a>                                    
+                                            </li>
+
+                                            <?php 
+                                                $notificationsCount = getPendingNotificationsCount($this);
+                                                if ($notificationsCount > 0)
+                                                {
+                                            ?>
+                                                    <li role="presentation">
+                                                        <a role="menuitem" tabindex="-1" href="<?php echo base_url().'notifications' ?>">
+
+                                                            
+                                                            <span class="badge badge-important"><?php echo $notificationsCount ?></span>
+                                                            Notifications Pending                                             
+                                                        
+
+                                                        </a>
+                                                    </li>
+                                            <?php
+                                                }
+                                            ?>
+
+                                            <li role="presentation" class="divider"></li>
+                                            <li role="presentation"><a role="menuitem" tabindex="-1" href="<?php echo base_url().'logout' ?>">Logout</a></li>
+                                        </ul>
+                                      </a>
+
+                                    </li>
+
+                                <?php } else { ?>
+
+                                    <?php echo get_nav_item('login', 'Login') ?> 
+
+                                <?php } ?>                    
+                                
                             </ul>
-                          </a>
-
-                        </li>
-
-                    <?php } else { ?>
-
-                        <?php echo get_nav_item('login', 'Login') ?> 
-
-                    <?php } ?>                    
-                    
-                </ul>
 
 
 
-                <?php echo form_open('searchcontroller/search_string', array( 
-                    'class' => 'navbar-search pull-right', 
-                    'id' => 'search-form-top',
-                    'method' => 'GET')) ?>
-                    <div class="input-append">
-                        <?php
+                            <?php echo form_open('searchcontroller/search_string', array( 
+                                'class' => 'navbar-search pull-right', 
+                                'id' => 'search-form-top',
+                                'method' => 'GET')) ?>
+                                <div class="input-append">
+                                    <?php
 
-                            $searchParam = '';
-                            $uri = uri_string();
-                            $pos = stripos($uri, 'search/');
-                            if (isset($pos) && $pos === 0)
-                            {
-                                $searchParam = urldecode(substr($uri, strlen('search/')));
-                            }
+                                        $searchParam = '';
+                                        $uri = uri_string();
+                                        $pos = stripos($uri, 'search/');
+                                        if (isset($pos) && $pos === 0)
+                                        {
+                                            $searchParam = urldecode(substr($uri, strlen('search/')));
+                                        }
 
-                            echo form_input(array(
-                                'id' => 'text-search-top',
-                                'name' => 'q',
-                                'type' => 'text',
-                                'class' => 'span2 search-query',
-                                'placeholder' => 'search for people, skills, projects and terms...',
-                                'required' => '',
-                                'title' => 'search criteria',
-                                'value' => $searchParam
-                            ));
-                            
-                            echo form_button(array(
-                                'id' => 'btn-search-top',
-                                'type' => 'Submit',
-                                'class' => 'btn btn-link',
-                                'content' => '<i class="icon-search"></i>'
-                            ));                             
+                                        echo form_input(array(
+                                            'id' => 'text-search-top',
+                                            'name' => 'q',
+                                            'type' => 'text',
+                                            'class' => 'span2 search-query',
+                                            'placeholder' => 'search for people, skills, projects and terms...',
+                                            'required' => '',
+                                            'title' => 'search criteria',
+                                            'value' => $searchParam
+                                        ));
+                                        
+                                        echo form_button(array(
+                                            'id' => 'btn-search-top',
+                                            'type' => 'Submit',
+                                            'class' => 'btn btn-link',
+                                            'content' => '<i class="icon-search"></i>'
+                                        ));                             
 
-                        ?>
+                                    ?>
+                                </div>
+                            <?php echo form_close() ?>     
+
                     </div>
-                <?php echo form_close() ?>     
-
-            <?php } ?>
+                <?php } ?>
+            </div>
           </div>
         </div>
 
