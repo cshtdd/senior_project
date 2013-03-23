50f5ffdd45f8472f9ef2f6206998b008efe0ccaf
Camilo Sanchez <camilin87@gmail.com>
2013-03-23 16:56:02 -0400

    some basic info adde to about page

--- application/views/staticpages_about.php
+++ application/views/staticpages_about.php
@@ -1,8 +1,32 @@
 <?php $this->load->view("template_header"); ?>
 
-<h2>Developers</h2>
-<p>Camilo Sanchez</p>
-<p>Keiser Moya</p>
-<p>Yaneli Fernandez</p>
+<h2>Senior Project Website</h2>
+<p>Developed to help FIU Computer Science students to choose their senior project. </p>
+<p>Performs an intelligent match between users and projects, determining the best project for each student.</p>
+<p>Allows students to invite other students and mentor to their projects</p>
+
+<ul>
+    <lh><h4>Developers</h4></lh>
+    <li>Camilo Sanchez <a href="https://twitter.com/camilin87">@camilin87</a> </li>
+    <li>Keiser Moya</li>
+    <li>Yaneli Fernandez</li>
+</ul>
+
+<ul>
+    <lh><h4>Technology Stack</h4></lh>
+    <li><a href="http://ellislab.com/codeigniter">CodeIgniter</a></li>
+    <li><a href="http://www.mysql.com">MySql</a></li>
+    <li><a href="http://git-scm.com">Git</a></li>
+    <li><a href="http://www.apache.org">Apache</a></li>
+</ul>
+
+
+<ul>
+    <lh><h4>More Info</h4></lh>
+    <li><a href="http://share.axure.com/6RZ1NQ/Login.html">Wireframes</a></li>
+    <li><a href="https://github.com/camilin87/senior_project/">Source Code Repository</a></li>
+    <li><a href="https://github.com/camilin87/senior_project_db/">Database Repository</a></li>
+    <li><a href="https://github.com/camilin87/senior_project/issues">Project Issues</a></li>
+</ul>
 
 <?php $this->load->view("template_footer"); ?>
