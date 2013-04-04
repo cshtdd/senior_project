1d976054ae7927c01977effda36ccc9ac5c8a6cc
Camilo Sanchez <camilin87@gmail.com>
2013-04-03 22:23:30 -0400

    setErrorFlashMessage displays an error flash message

--- application/helpers/flash_message_helper.php
+++ application/helpers/flash_message_helper.php
@@ -44,11 +44,42 @@ if ( !function_exists('getFlashMessage'))
     }
 }
 
+if ( !function_exists('getFlashMessageClass'))
+{
+    function getFlashMessageClass($sender_controller)
+    {
+        if (is_test($sender_controller))
+        {
+            //return 'This is a flash message';
+        }
+
+        $flashMessageClass = $sender_controller->session->flashdata('FLASH_MESSAGE_CLASS');
+
+        if (isset($flashMessageClass) && strlen($flashMessageClass) > 0)
+        {
+            return $flashMessageClass;
+        }
+        else
+        {
+            return '';
+        }
+    }
+}
+
 if ( !function_exists('setFlashMessage'))
 {
-    function setFlashMessage($sender_controller, $msg)
+    function setFlashMessage($sender_controller, $msg, $msg_class='')
     {
         $sender_controller->session->set_flashdata('FLASH_MESSAGE', $msg);
+        $sender_controller->session->set_flashdata('FLASH_MESSAGE_CLASS', $msg_class);
+    }
+}
+
+if ( !function_exists('setErrorFlashMessage'))
+{
+    function setErrorFlashMessage($sender_controller, $msg)
+    {
+        setFlashMessage($sender_controller, $msg, 'alert-error');
     }
 }
 
