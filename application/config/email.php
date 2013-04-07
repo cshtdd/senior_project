a678d7612c8faa626112403d6d133e82d2021793
Camilo Sanchez <camilin87@gmail.com>
2013-04-06 22:18:16 -0400

    send email

---
+++ application/config/email.php
@@ -0,0 +1,9 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+//make sure so set a default timezone in the php.ini file
+//date.timezone = America/New_York
+
+$config['protocol'] = 'sendmail';
+//$config['smtp_host'] = '';
+//$config['smtp_user'] = '';
+//$config['smtp_pass'] = '';
